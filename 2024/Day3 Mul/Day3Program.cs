726db6aee3a4505b9683ba04fd214629b36ce2b1
erkan <etaylan13@gmail.com>
2024-12-25 13:24:28 +0300

    done: 2024 Day 3 Part 2

--- 2024/Day3 Mul/Day3Program.cs
+++ 2024/Day3 Mul/Day3Program.cs
@@ -10,6 +10,42 @@ internal static partial class Day3Program
     {
         string input = File.ReadAllText(InputPath);
 
+        MatchCollection matches = MatchMulsDosDonts().Matches(input);
+
+        var sum = 0;
+        var isDo = true;
+        foreach (Match match in matches)
+        {
+            var value = match.ToString();
+
+            if (value == "do()")
+            {
+                isDo = true;
+            }
+            else if (value == "don't()")
+            {
+                isDo = false;
+            }
+            else
+            {
+                if (isDo)
+                {
+                    (int left, int right) = ParseLeftRight(value);
+                    sum += left * right;
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
+        //108830766
+
+
+        //CalculatePart1(matches);
+    }
+
+
+    private static void CalculatePart1(string input)
+    {
         MatchCollection matches = MatchMuls().Matches(input);
 
         var sum = 0;
@@ -17,9 +53,7 @@ internal static partial class Day3Program
         foreach (Match match in matches)
         {
             var value = match.ToString();
-            string[] clear = value.Replace("mul(", "").Replace(")", "").Split(',');
-            int left = int.Parse(clear[0]);
-            int right = int.Parse(clear[1]);
+            (int left, int right) = ParseLeftRight(value);
             sum += left * right;
         }
 
@@ -27,6 +61,18 @@ internal static partial class Day3Program
         // 165225049
     }
 
+    private static (int left, int right) ParseLeftRight(string mul)
+    {
+        string[] clear = mul.Replace("mul(", "").Replace(")", "").Split(',');
+        int left = int.Parse(clear[0]);
+        int right = int.Parse(clear[1]);
+        return (left, right);
+    }
+
     [GeneratedRegex(@"mul\((-?\d+),(-?\d+)\)")]
     private static partial Regex MatchMuls();
+
+
+    [GeneratedRegex(@"(mul\((-?\d+),(-?\d+)\))|(don't\(\))|(do\(\))")]
+    private static partial Regex MatchMulsDosDonts();
 }
